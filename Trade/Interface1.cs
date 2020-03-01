549ffdd719be37640c86573e2b17dd28957cea26
Александр <2013gologramma@gmail.com>
2020-03-01 14:00:43 +0300

    Второй вариант

---
+++ Trade/Interface1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Trade
+{
+    interface Interface1
+    {
+
+        string TextPage { set; get; }
+
+        string Picture1 { set; get; }
+        string Picture { get; set; }
+
+
+    }
+}
