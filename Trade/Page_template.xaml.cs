4e1a9054160e66c3ca0b5b8870afbfc86a3312e8
Александр <2013gologramma@gmail.com>
2020-03-07 22:21:45 +0300

    простое сохранение

--- Trade/Page_template.xaml.cs
+++ Trade/Page_template.xaml.cs
@@ -7,30 +7,70 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
-namespace Trade  
+namespace Trade
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Page_template : ContentPage  // Шаблон для создания страницы с текстом. 
     {
+        int PageContent = 0;
         public Page_template(int page)
         {
+            
             NavigationPage.SetHasNavigationBar(this, false);
-           
-             InitializeComponent();
+            PageContent = page;
+            InitializeComponent();
 
-           Str str1 = new Str(page);
-
+            Str str1 = new Str(page);
+            
             foreach (var lab in str1.Lcontent)
             {
-                 ContPage.Children.Add(lab);
+                ContPage.Children.Add(lab);
             }
+            
            
+
         }
 
-       
+        private void IncreaseFont_Clicked(object sender, EventArgs e)
+        {
+            int ddd = ContPage.Children.Count;
+            
+            foreach (View item in ContPage.Children)
+            {
+              Type type =  item.GetType();
+                if (type == typeof(Label))
+                {
+                   
+                }
+            }
 
-       
-        
+
+            IncreaseFont.Text = "uuuuuu";
+            Titl.FontSize = 40;
+
+
+
+
+
+        }
+        private void ReduceFont_Clicked(object sender, EventArgs e)
+        {
+            //if (GlobalValue.MyFontSizeLabel >5)
+            //{
+            //    GlobalValue.MyFontSizeLabel--;
+            //    GlobalValue.MyFontSizeTitle--;
+            //}
+
+            //Str str1 = new Str(PageContent);
+            //foreach (var lab in str1.Lcontent)
+            //{
+            //    ContPage.Children.Add(lab);
+            //}
+
+
+        }
+
+
 
     }
 }
